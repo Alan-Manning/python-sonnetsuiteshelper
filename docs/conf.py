ab4f0bbb4afaa5f1c08b5c3243af3efea6e2568f
Alan Manning <Alan_Manning@Live.co.uk>
2025-06-10 22:12:26 +0100

    Bump version: 1.0.4 → 1.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ project = "sonnetsuiteshelper"
 year = "2024"
 author = "Alan Manning"
 copyright = f"{year}, {author}"
-version = release = "1.0.4"
+version = release = "1.0.5"
 
 pygments_style = "trac"
 templates_path = ["."]
