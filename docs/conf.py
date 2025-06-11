ae45a1a368b73159e79adfc46139fa1b111782b3
Alan Manning <Alan_Manning@Live.co.uk>
2025-06-11 13:43:05 +0100

    Bump version: 1.0.14 → 1.0.15

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ project = "sonnetsuiteshelper"
 year = "2024"
 author = "Alan Manning"
 copyright = f"{year}, {author}"
-version = release = "1.0.14"
+version = release = "1.0.15"
 
 pygments_style = "trac"
 templates_path = ["."]
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*names, **kwargs):
 
 setup(
     name="sonnetsuiteshelper",
-    version="1.0.14",
+    version="1.0.15",
     license="LGPL-3.0-or-later",
     description="A package to help with Sonnet Suites.",
     long_description="{}\n{}".format(
